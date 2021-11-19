dc8dc76cc1e5e3257e93babbf5a08ce4dffd8c4d
KunalKatiyar <katiyarkunal2011@gmail.com>
2021-11-19 16:26:13 +0530

    Added Particles

--- Scenes/Enemies/WindowsDefender.gd
+++ Scenes/Enemies/WindowsDefender.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
 onready var path_follow = get_parent()
-
+signal die
 export var _speed = 0
 
 func _physics_process(delta):
 	path_follow.set_offset(path_follow.get_offset() + _speed * delta)
+
+
+func _on_Area2D2_body_entered(body):
+	if body.name == "Player":
+		emit_signal("die")
--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -32,31 +32,72 @@
 
 extends KinematicBody2D
 
+
+const ACC = 2048
 const MOVE_SPEED = 500
 const JUMP_FORCE = 1000
 const GRAVITY = 50
 const MAX_FALL_SPEED = 1000
+const FRICTION = 0.25
+const AIR_RESIST = 0.05
 var JUMPED = 0
-
+var motion = 0
 var y_velo = 0
+var NUMOFJUMPS = 2
 var facing_right = false
+var dead = false
 
 func _physics_process(_delta):
-	var move_dir = 0
-	if Input.is_action_pressed("ui_right"):
-		move_dir += 1
-	if Input.is_action_pressed("ui_left"):
-		move_dir -= 1
-	
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
-	
-	var grounded = is_on_floor()
-	y_velo += GRAVITY
-	if JUMPED < 2 and Input.is_action_just_pressed("ui_up"):
-		JUMPED += 1
-		y_velo = -JUMP_FORCE
-	if grounded and y_velo >= 0:
-		y_velo = 5
-		JUMPED = 0
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
+	if not dead:
+		var move_dir = 0
+	#	print(get_node("MovementParticles").process_material.initial_velocity)
+		if Input.is_action_pressed("ui_right"):
+			move_dir += 1
+		if Input.is_action_pressed("ui_left"):
+			move_dir -= 1
+		if move_dir != 0:
+			motion += move_dir * ACC * _delta
+			motion = clamp(motion, -MOVE_SPEED, MOVE_SPEED)
+			
+		var grounded = is_on_floor()
+		if move_dir == 0:
+			get_node("MovementParticles").emitting = false
+		else:
+			get_node("MovementParticles").emitting = true
+			if move_dir == 1:
+				get_node("MovementParticles").process_material.initial_velocity = abs(motion)/10 * _delta
+				get_node("MovementParticles").set_rotation_degrees(-180)
+			if move_dir == -1:
+				get_node("MovementParticles").process_material.initial_velocity = abs(motion)/10 * _delta
+				get_node("MovementParticles").set_rotation_degrees(0)
+			
+		if grounded:
+			if move_dir == 0:
+				motion = lerp(motion, 0, FRICTION)
+		else:
+			if Input.is_action_just_released("ui_up") and y_velo < -JUMP_FORCE/2:
+				y_velo = -JUMP_FORCE/2
+			if move_dir == 0:
+				motion = lerp(motion, 0, AIR_RESIST)
+				
+		move_and_slide(Vector2(motion, y_velo), Vector2(0, -1))
+		y_velo += GRAVITY
+		if JUMPED < NUMOFJUMPS and Input.is_action_just_pressed("ui_up"):
+			JUMPED += 1
+			y_velo = -JUMP_FORCE
+		if grounded and y_velo >= 0:
+			y_velo = 5
+			JUMPED = 0
+		if y_velo > MAX_FALL_SPEED:
+			y_velo = MAX_FALL_SPEED
+
+func _on_WindowsDefender_die():
+	NUMOFJUMPS = 3
+	dead = true
+	get_node("MovementParticles").emitting = false
+	$"AnimationPlayer".play("death")
+	yield(get_tree().create_timer(0.2), "timeout")
+	get_node("DeathParticles").emitting = true
+	yield(get_tree().create_timer(1), "timeout")
+	get_tree().change_scene("res://Scenes/Levels/Level1.tscn")
+#	$"Sprite/AnimatedSprite".play("default")
