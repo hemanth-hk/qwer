208887a39425cb2c553afa23986736df3090727b
hemanth-hk <uppadahemanthkumar27@gmail.com>
2021-12-01 18:14:31 +0530

    dio dialogues

---
+++ Scripts/Story/end.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+#	var new_dialog = Dialogic.start('gunguy')
+#	add_child(new_dialog)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
