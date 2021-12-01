a971b277ef70edd20499779c0c3148d1148628fc
KunalKatiyar <katiyarkunal2011@gmail.com>
2021-12-01 18:11:58 +0530

    Done

--- Scripts/Hazards/Wall.gd
+++ Scripts/Hazards/Wall.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 func _on_Area2D_area_entered(area):
 	print(area.name)
-	if "PlayerBullet" in area.name:
+	if "PlayerBullet" in area.name and Variables.powers[2]:
 		$AnimationPlayer.play("destroy")
 		yield(get_tree().create_timer(1.5), "timeout")
 		queue_free()
